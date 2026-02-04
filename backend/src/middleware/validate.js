f99358d60451fcb65399d2de8a558e1eec524f78
Kuy Visal <kuyvisal413@gmail.com>
2026-02-04 16:36:17 +0700

    change the min 6 to min 8

--- backend/src/middleware/validate.js
+++ backend/src/middleware/validate.js
@@ -7,7 +7,7 @@ export const validateLogin = [
 
   body('password')
     .notEmpty().withMessage('Password is required')
-    .isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+    .isLength({ min: 8 }).withMessage('Password must be at least 8 characters'),
 
   (req, res, next) => {
     const errors = validationResult(req)
--- backend/src/routes/authenroutes.js
+++ backend/src/routes/authenroutes.js
@@ -19,28 +19,3 @@ router.get('/users/:id/profile', getProfileById)
 
 export default router
 
-// src/routes/authRoutes.js
-// import express from 'express'
-// import { auth } from '../auth.js'
-
-// const authRoutes = express.Router()
-
-// authRoutes.post('/register', async (req, res) => {
-//   try {
-//     const user = await auth.emailAndPassword.register(req.body)
-//     res.status(201).json(user)
-//   } catch (error) {
-//     res.status(400).json({ message: error.message })
-//   }
-// })
-
-// authRoutes.post('/login', async (req, res) => {
-//   try {
-//     const result = await auth.emailAndPassword.login(req.body)
-//     res.json(result)
-//   } catch (error) {
-//     res.status(400).json({ message: error.message })
-//   }
-// })
-
-// export default authRoutes
